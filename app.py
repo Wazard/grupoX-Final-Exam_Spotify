ec0e32f665c2a7c66bc4caa6d6a23a25489522d1
Wazard <lucasartorelli@outlook.it>
2025-12-16 17:31:59 +0100

    removed BaseDataHandler from app. replaced with pandas

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-from my_utils.base_data_handler import BaseDataHandler  # type: ignore
 from recommender.cold_start import generate_cold_start_songs
 from features.song_representation import vectorize_song
 from evaluation.metrics import get_similarity
 from user.profile import UserProfile
+import pandas as pd
+
+FILE_PATH = "data/processed/tracks_processed_normalized.csv"
 
 user_profile = UserProfile.load()
-
-handler = BaseDataHandler("data/processed/tracks_processed_normalized.csv")
+df = pd.read_csv(FILE_PATH, encoding='utf-8')
 
 if not user_profile.has_profile():
     # --- Cold start ---
-    cold_start_songs = generate_cold_start_songs(handler.df, 20)
+    cold_start_songs = generate_cold_start_songs(df, 20)
 
     print("\nCold start songs:")
     print(cold_start_songs[[
@@ -34,7 +35,7 @@ if not user_profile.has_profile():
     user_profile.save()
 
 # --- Similarity sanity check ---
-song_a, _ = handler.df.sample(n=2).iloc
+song_a, _ = df.sample(n=2).iloc
 
 vector_song_a = vectorize_song(song_a)
 similarity = get_similarity(user_profile.get_profile_vector(), vector_song_a)
